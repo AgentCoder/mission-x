646219ea6fee605f5e8f364e44db3d2109153fae
Surya sai Karteek Kuppili <S525038@nwmissouri.edu>
2016-09-15 16:26:31 -0500

    Designed UI for Trigonometric Problems

---
+++ src/Mission X/Controllers/TrigonometryController.cs
@@ -0,0 +1,13 @@
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+
+namespace MissionX.Controllers
+{
+    public class TrigonometryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
