e6e2acc887e836a077c80d2533c912085a364bee
Pranay_Peddi <S525083@mail.nwmissouri.edu>
2016-09-15 13:40:38 -0500

    Added Tool Tips to the home dashboard. Made UI corrections.

--- src/Mission X/wwwroot/js/site.js
+++ src/Mission X/wwwroot/js/site.js
@@ -1,1 +1,20 @@
-﻿// Write your Javascript code.
+﻿
+
+document.querySelector('#p3').addEventListener('mdl-componentupgraded', function () {
+    this.MaterialProgress.setProgress(10);
+
+});
+document.querySelector('#p4').addEventListener('mdl-componentupgraded', function () {
+    this.MaterialProgress.setProgress(25);
+    
+});
+
+document.querySelector('#p5').addEventListener('mdl-componentupgraded', function () {
+    this.MaterialProgress.setProgress(10);
+   
+});
+
+document.querySelector('#projectstatus').addEventListener('mdl-componentupgraded', function () {
+    this.MaterialProgress.setProgress(25);
+
+});
