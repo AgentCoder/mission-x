0b3844029f0cec910d64571f0ac340f94514b661
shravyach <S524985@mail.nwmissouri.edu>
2016-10-30 18:03:30 -0500

    Added dynamic text fields and validated Three point problem.

--- src/Mission X/wwwroot/js/3Point.js
+++ src/Mission X/wwwroot/js/3Point.js
@@ -24,4 +24,13 @@
 
         document.getElementById('puli').innerHTML = strike
 
+}
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
     }
+    return true;
+}
+
