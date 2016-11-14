788e130cf0c2b4e9bf3ab4ee57a82845f0b77a96
S524985 <S524985@NW33939>
2016-11-13 21:47:46 -0600

    Completed the validations of 3-point problem

--- src/Mission X/wwwroot/js/3Point.js
+++ src/Mission X/wwwroot/js/3Point.js
@@ -5,6 +5,44 @@
     var point1 = document.getElementById("pointB").value;
     var point2 = document.getElementById("pointC").value;
     var dista = document.getElementById("dist").value;
+    if (point == null || point == "", point1 == null || point1 == "", point2 == null || point2 == "", dista == null || dista == "") {
+        alert("Please Fill All Required Fields");
+        return false;
+    }
+    if (point == null || point == "" || point == 0)
+    {
+        alert("The first point is not appropriate");
+        return false;
+        
+    }
+    if (point1 == null || point1 == "" || point1 == 0) {
+        alert("The second point is not appropriate");
+        return false;
+
+    }
+    if (point2 == null || point2 == "" || point2 == 0) {
+        alert("The third point is not appropriate");
+        return false;
+
+    }
+    if(point==point1 || point==point2 || point == dista)
+    {
+        alert("Please check the first point again");
+        return false;
+    }
+    if(point1== point2 || point1==dista){
+        alert("Please check the second point again");
+        return false;
+    }
+    if(point2==dista){
+        alert("Please check the third point again");
+        return false;
+    }
+    if (dista == null || dista == "" || dista == 0) {
+        alert("The distance is not appropriate");
+        return false;
+
+    }
     //difference between point B and A
         var subdist = point1 - point;
         var x = subdist / dista; 
@@ -23,6 +61,5 @@
         } else {
             var strike = point11 - point1;
         }
-        
         param(appdip);
 }
